157cafea0e471501c5a0d3e760d13432ec026f12
slazaryan <slazaryan@phoenixit.ru>
2021-10-03 12:37:47 +0300

    add view front

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,17 @@
 import Fastify, { FastifyInstance } from 'fastify'
+import FastifyStatic from 'fastify-static'
+
+import path from 'path';
 
 const server: FastifyInstance = Fastify({logger: true })
+
+server.register(FastifyStatic, {
+  root: path.join(__dirname, '../front/dist/')
+})
+
+server.get('/', (req, repl) => {
+  repl.sendFile('index.html')
+})
 
 const start = async () => {
   try {
