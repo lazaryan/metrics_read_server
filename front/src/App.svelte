041a406032720cc1466e7bfc25fb7032bba17076
slazaryan <slazaryan@phoenixit.ru>
2021-10-03 12:09:31 +0300

    initial project

---
+++ front/src/App.svelte
@@ -0,0 +1,13 @@
+<svelte:options tag="sl-widget-metrics"/>
+
+<script lang="ts">
+
+</script>
+
+<main>
+  Initial widget
+</main>
+
+<style>
+  
+</style>
---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import Fastify, { FastifyInstance } from 'fastify'
+
+const server: FastifyInstance = Fastify({logger: true })
+
+const start = async () => {
+  try {
+    await server.listen(3000)
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  } 
+}
+
+start()
